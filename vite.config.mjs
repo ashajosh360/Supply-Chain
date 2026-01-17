22a710b7735460e0e0f752de8d15c69b896733c6
Joshua 360 <47924907+ashajosh360@users.noreply.github.com>
2026-01-17 10:23:56 +0100

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -9,7 +9,7 @@ export default defineConfig({
   // comment this out if that isn't relevant for your project
   build: {
     outDir: "build",
-    chunkSizeWarningLimit: 2000,
+    chunkSizeWarningLimit: 20000,
   },
   plugins: [tsconfigPaths(), react(), tagger()],
   server: {
